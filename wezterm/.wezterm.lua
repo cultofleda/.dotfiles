444a2f5a841ef818ef26204f84e6283c17226247
cultofleda <artur.vazigin@gmail.com>
2024-01-30 09:01:59 +0100

    wezterm: change material theme style

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,7 +5,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "Material (base16)"
+config.color_scheme = "MaterialDarker"
 config.font = wezterm.font("JetBrainsMono Nerd Font Propo", {
 	weight = "Medium",
 })
