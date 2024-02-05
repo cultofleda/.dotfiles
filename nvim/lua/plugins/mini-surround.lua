715c2116acab6318518b2b8c236931342bd24dfb
cultofleda <artur.vazigin@gmail.com>
2024-02-05 13:55:29 +0100

    nvim: add custom surround script

--- nvim/lua/plugins/mini-surround.lua
+++ nvim/lua/plugins/mini-surround.lua
@@ -10,5 +10,19 @@ return {
       replace = "gsr",
       update_n_lines = "gsn",
     },
+    custom_surroundings = {
+      ["<"] = {
+        input = function()
+          local tag = MiniSurround.user_input("Tag to wrap selection in: ")
+          return { tag .. "().-()" .. tag }
+        end,
+        output = function()
+          local tag = MiniSurround.user_input("Tag to wrap selection in: ")
+          local left = string.format("<%s>", tag)
+          local right = string.format("</%s>", tag)
+          return { left = left, right = right }
+        end,
+      },
+    },
   },
 }
