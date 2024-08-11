aa3125e3f9e1ed8246b2153c800bdaef434ab8e0
cultofleda <artur.vazigin@gmail.com>
2024-08-11 20:20:57 +0200

    wezterm: rename config file
    make things more stow-friendly

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "nord"
+config.color_scheme = "Vs Code Dark+ (Gogh)"
 config.font = wezterm.font("JetBrainsMono Nerd Font Propo", {
 	weight = "Medium",
 })
