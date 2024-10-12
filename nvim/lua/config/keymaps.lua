50f654126e3c2d8476b811d81542387ecc821795
cultofleda <info@cultofleda.studio>
2024-10-12 13:14:28 +0200

    do loads of things idk

--- nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("n", "<leader>so", function()
-  require("telescope.builtin").lsp_document_symbols()
-end, { desc = "[S]earch [O]bjects (class and function)" })
